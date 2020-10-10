da929ee5ab8c9b0b34a640ce70aa48a7d505e560
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2020-10-10 21:13:06 +0530

    Login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,166 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Fluttern"),
+      ),
+      body: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/homeback.jpg',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: new EdgeInsets.all(20.0),
+                width: width,
+                height: height * 0.20,
+                child: CircleAvatar(
+                  radius: 70,
+                  backgroundColor: Colors.black,
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage(
+                      'assets/loginimg.jpg',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 50,
+                    width: width * 0.93,
+                    margin: new EdgeInsets.all(10.0),
+                    child: new TextFormField(
+                      autofocus: false,
+                      decoration: new InputDecoration(
+                        labelText: "Enter Mobile Number",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(
+                            25.0,
+                          ),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val.length == 0 || val.length < 10) {
+                          return "Enter a Valid Mobile Number";
+                        } else {
+                          return null;
+                        }
+                      },
+                      keyboardType: TextInputType.phone,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 50,
+                    width: width * 0.93,
+                    margin: new EdgeInsets.all(10.0),
+                    child: new TextFormField(
+                      obscureText: true,
+                      autofocus: false,
+                      decoration: new InputDecoration(
+                        labelText: "Enter Password",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(
+                            25.0,
+                          ),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val.length == 0 || val.length < 10) {
+                          return "Enter a Valid Mobile Number";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: width * 0.8,
+                    child: RaisedButton(
+                      color: Colors.blueAccent,
+                      textColor: Colors.white,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 30.0, fontFamily: "Lustria"),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "Already Login?",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Lustria",
+                    ),
+                  ),
+                  InkWell(
+                    child: new Text(
+                      "Register Here",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onTap: () {},
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
